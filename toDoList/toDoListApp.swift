d6e78863279cdb29f618c90c46a9cf2edeadd043
hacesolluna <sunny.moonsaic@gmail.com>
2023-08-01 10:39:21 -0700

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Sunny Moon on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
